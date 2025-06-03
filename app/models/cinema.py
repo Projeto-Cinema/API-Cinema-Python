f2b2f89dcad879f15e152f4be920ba4280b83aa7
Weslin-0101 <wesleeylirac12@gmail.com>
2025-06-03 08:42:36 -0300

    feat(app): removing the Sala class from Cinema and creating its own class

--- app/models/cinema.py
+++ app/models/cinema.py
@@ -18,16 +18,3 @@ class Cinema(BaseModel):
     endereco = relationship("Endereco", back_populates="cinema")
     salas = relationship("Sala", back_populates="cinema", cascade="all, delete-orphan")
     produtos = relationship("Produto", back_populates="cinema", cascade="all, delete-orphan")
-
-class Sala(BaseModel):
-    cinema_id = Column(Integer, ForeignKey("cinema.id"), nullable=False)
-    nome = Column(String(50), nullable=False)
-    capacidade = Column(Integer, nullable=False)
-    tipo = Column(String(20), nullable=False)
-    recursos = Column(String(255), nullable=True) # Vai armazenar a lista em formato JSON
-    mapa_assentos = Column(Text, nullable=True) # Vai armazenar o mapa de assentos em formato JSON
-    status = Column(String(20), nullable=False, default="ativo") # ativo, em_manutencao, inativo
-
-    # Relacionamentos
-    cinema = relationship("Cinema", back_populates="salas")
-    sessoes = relationship("Sessao", back_populates="sala", cascade="all, delete-orphan")
---
+++ app/models/sala.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, Text
+from sqlalchemy.orm import relationship
+
+from app.models.base import BaseModel
+
+class Sala(BaseModel):
+    cinema_id = Column(Integer, ForeignKey("cinema.id"), nullable=False)
+    nome = Column(String(50), nullable=False)
+    capacidade = Column(Integer, nullable=False)
+    tipo = Column(String(20), nullable=False)
+    recursos = Column(String(255), nullable=True) # Vai armazenar a lista em formato JSON
+    mapa_assentos = Column(Text, nullable=True) # Vai armazenar o mapa de assentos em formato JSON
+    status = Column(String(20), nullable=False, default="ativo") # ativo, em_manutencao, inativo
+
+    # Relacionamentos
+    cinema = relationship("Cinema", back_populates="salas")
+    sessoes = relationship("Sessao", back_populates="sala", cascade="all, delete-orphan")
