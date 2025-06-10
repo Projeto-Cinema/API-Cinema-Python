112edbf84e28dd9d0802ebb29668021b58b3d8bb
Weslin-0101 <wesleeylirac12@gmail.com>
2025-06-10 14:54:10 -0300

    feat(app): adjusting the imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,10 +1,14 @@
 from app.models.base import BaseModel
-from app.models.usuario import Usuario, Endereco
-from app.models.cinema import Cinema, Sala
+from app.models.usuario import Usuario
+from app.models.endereco import Endereco
+from app.models.cinema import Cinema
+from app.models.sala import Sala
 from app.models.filme import Filme
-from app.models.sessao import Sessao, Assento
+from app.models.sessao import Sessao
+from app.models.assento import Assento
 from app.models.reserva import Reserva, ItemReserva
-from app.models.Produto import Produto, Pagamento
+from app.models.Produto import Produto
+from app.models.pagamento import Pagamento
 
 # Exporta todos os modelos para facilitar importações
 __all__ = [
