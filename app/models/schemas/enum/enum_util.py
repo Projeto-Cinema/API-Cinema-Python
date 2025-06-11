afa14278e66c8375215395e0f54f1bcda4957041
Weslin-0101 <wesleeylirac12@gmail.com>
2025-06-10 23:09:52 -0300

    refactor(app): adjusting the enums

--- app/models/schemas/enum/enum_util.py
+++ app/models/schemas/enum/enum_util.py
@@ -2,27 +2,27 @@ from enum import Enum
 
 
 class StatusReservaEnum(str, Enum):
-    PENDENTE = "Pendente"
-    CONFIRMADA = "Confirmada"
-    CANCELADA = "Cancelada"
-    EXPIRADA = "Expirada"
+    PENDENTE = "pendente"
+    CONFIRMADA = "confirmado"
+    CANCELADA = "cancelada"
+    EXPIRADA = "expirado"
 
 class TipoItemEnum(str, Enum):
-    ASSENTO = "Assento"
-    PRODUTO = "Produto"
+    ASSENTO = "assento"
+    PRODUTO = "produto"
 
 class StatusSessaoEnum(str, Enum):
-    ATIVA = "Ativa"
-    INATIVA = "Inativa"
-    CANCELADA = "Cancelada"
+    ATIVA = "ativa"
+    ENCERRADA = "encerrada"
+    CANCELADA = "cancelada"
 
 class StatusSalaEnum(str, Enum):
-    ATIVA = "Ativa"
-    INATIVA = "Inativa"
-    CANCELADA = "Cancelada"
+    ATIVA = "ativo"
+    MANUTENCAO = "em_manutencao"
+    INATIVO = "inativo"
 
 class StatusAssentoEnum(str, Enum):
-    DISPONIVEL = "Disponível"
-    RESERVADO = "Reservado"
-    OCUPADO = "Ocupado"
-    INDISPONIVEL = "Indisponível"
+    DISPONIVEL = "disponivel"
+    RESERVADO = "reservado"
+    OCUPADO = "ocupado"
+    INDISPONIVEL = "indisponivel"
