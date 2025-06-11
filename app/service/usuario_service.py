980e6cdb46ff8de5f6228a62289b8044dc36cb22
Weslin-0101 <wesleeylirac12@gmail.com>
2025-06-11 08:53:41 -0300

    feat(app): implementing UsuarioService

---
+++ app/service/usuario_service.py
@@ -0,0 +1,53 @@
+from fastapi import HTTPException, status
+from passlib.context import CryptContext
+
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+
+from app.models.schemas.usuario_schema import UsuarioCreate
+from app.models.usuario import Usuario
+
+class UsuarioService:
+    def __init__(self):
+        self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def _hash_password(self, password: str) -> str:
+        return self.pwd_context.hash(password)
+
+    def _verify_password(self, plain_password: str, hashed_password: str) -> str:
+        return self.pwd_context.verify(plain_password, hashed_password)
+    
+    def create_user(self, db: Session, usuario_data: UsuarioCreate) -> Usuario:
+        try:
+            hashed_password = self._hash_password(usuario_data.senha)
+
+            usuario_dict = usuario_data.model_dump(exclude={'senha'})
+            usuario_dict['senha'] = hashed_password
+
+            db_usuario = Usuario(**usuario_dict)
+            db.add(db_usuario)
+            db.commit()
+            db.refresh(db_usuario)
+
+            return db_usuario
+        
+        except IntegrityError as e:
+            db.rollback()
+
+            if "email" in str(e.orig):
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="Email já cadastrado."
+                )
+            elif "cpf" in str(e.orig):
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="CPF já cadastrado."
+                )
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    detail="Erro ao criar usuário."
+                )
+            
+usuario_service = UsuarioService()
