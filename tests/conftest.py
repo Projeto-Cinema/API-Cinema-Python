87a8df3eeaa25f49df68964e878bdb26b4a6c03e
Weslin-0101 <wesleeylirac12@gmail.com>
2025-06-19 15:27:55 -0300

    refactor(app): moving the fixtures to the conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
 from fastapi.testclient import TestClient
+from fastapi import status
+
 import pytest
+
 from setuptools import find_packages
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -50,3 +53,33 @@ def db_session():
         yield db
     finally:
         db.close()
+
+@pytest.fixture(scope="session")
+def endereco_payload():
+    return {
+        "cep": "12345678",
+        "logradouro": "Rua Exemplo",
+        "numero": 123,
+        "bairro": "Bairro Exemplo",
+        "cidade": "Cidade Exemplo",
+        "estado": "EX",
+        "complemento": "Apto 1",
+        "referencia": "ref exemplo"
+    }
+
+@pytest.fixture(scope="function")
+def create_address(client, endereco_payload):
+    response = client.post("api/v1/address", json=endereco_payload)
+    assert response.status_code == status.HTTP_201_CREATED
+    return response.json()
+
+@pytest.fixture(scope="function")
+def cinema_data(create_address):
+    return {
+        "nome": "Cinema Exemplo",
+        "cnpj": "12.345.678/0001-90",
+        "telefone": "1234567890",
+        "email": "cinema@gmail.com",
+        "ativo": True,
+        "endereco_id": create_address["id"]
+    }
--- tests/integration/test_cinema_controller.py
+++ tests/integration/test_cinema_controller.py
@@ -4,37 +4,7 @@ from fastapi import status
 
 class TestCinemaController:
 
-    @pytest.fixture
-    def endereco_data(self):
-        return {
-            "cep": "12345678",
-            "logradouro": "Rua Exemplo",
-            "numero": 123,
-            "bairro": "Bairro Exemplo",
-            "cidade": "Cidade Exemplo",
-            "estado": "EX",
-            "complemento": "Apto 1",
-            "referencia": "ref exemplo"
-        }
-    
-    @pytest.fixture
-    def create_address(self, client, endereco_data):
-        response = client.post("api/v1/address", json=endereco_data)
-        assert response.status_code == status.HTTP_201_CREATED
-        return response.json()
-    
-    @pytest.fixture
-    def cinema_data(self, create_address):
-        return {
-            "nome": "Cinema Exemplo",
-            "cnpj": "12.345.678/0001-90",
-            "telefone": "1234567890",
-            "email": "cinema@gmail.com",
-            "ativo": True,
-            "endereco_id": create_address["id"]
-        }
-    
-    def test_create_cinema_integration_success(self, client, cinema_data, create_address):
+    def test_create_cinema_integration_success(self, client, cinema_data):
         response = client.post("/api/v1/cinema", json=cinema_data)
 
         assert response.status_code == status.HTTP_201_CREATED
@@ -42,4 +12,4 @@ class TestCinemaController:
         response_data = response.json()
         assert response_data["nome"] == cinema_data["nome"]
         assert response_data["cnpj"] == cinema_data["cnpj"]
-        assert response_data["endereco_id"] == create_address["id"]
+        assert response_data["endereco_id"] == cinema_data["endereco_id"]
