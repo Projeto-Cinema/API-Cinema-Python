f6a0d1ef0e638678ff8cc8c5f68aeb0a7a8957c6
Weslin-0101 <wesleeylirac12@gmail.com>
2025-06-23 23:54:17 -0300

    feat(app): implementing add item reserve route into controller and service

--- app/controllers/item_reserva_controller.py
+++ app/controllers/item_reserva_controller.py
@@ -1,10 +1,10 @@
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from sqlalchemy.orm import Session
 
 from app.database import get_db
-from app.models.schemas.item_reserva_schema import ItemReservaResponse
+from app.models.schemas.item_reserva_schema import ItemReservaCreate, ItemReservaResponse
 
 from app.service.item_reserva_service import item_reserva_service
 
@@ -26,3 +26,22 @@ def search_item_reserve(
     itens = item_reserva_service.search_item_by_id(db, reserve_id)
 
     return [ItemReservaResponse.from_orm(item) for item in itens]
+
+@router.post(
+    "/{reserve_id}/itens",
+    response_model=ItemReservaResponse,
+    status_code=status.HTTP_201_CREATED,
+    summary="Adicionar Item Reserva",
+    description="Adiciona um item reserva a uma reserva existente.",
+)
+def add_item_reserve(
+    reserve_id: int,
+    item_data: ItemReservaCreate,
+    db: Session = Depends(get_db)       
+):
+    try:
+        item = item_reserva_service.add_item_reserve(reserve_id, item_data, db)
+        return ItemReservaResponse.from_orm(item)
+    
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
--- app/service/item_reserva_service.py
+++ app/service/item_reserva_service.py
@@ -3,7 +3,9 @@ from passlib.context import CryptContext
 
 from sqlalchemy.orm import Session
 
-from app.models.reserva import ItemReserva
+from app.models.reserva import ItemReserva, Reserva
+from app.models.schemas.enum.enum_util import StatusReservaEnum
+from app.models.schemas.item_reserva_schema import ItemReservaCreate
 
 class ItemReservaService:
     def __init__(self):
@@ -11,5 +13,38 @@ class ItemReservaService:
 
     def search_item_by_id(self, db: Session, item_id: int) -> Optional[ItemReserva]:
         return db.query(ItemReserva).filter(ItemReserva.id == item_id).first()
+    
+    def add_item_reserve(
+        self,
+        reserve_id: int,
+        item_data: ItemReservaCreate,
+        db: Session
+    ) -> Optional[ItemReserva]:
+        reserve = db.query(Reserva).filter(Reserva.id == reserve_id).first()
+        if not reserve:
+            raise ValueError("Reserva não encontrada")
+        
+        if reserve.status != StatusReservaEnum.PENDENTE.value:
+            raise ValueError("Não é possível adicionar itens pendentes")
+        
+        item = ItemReserva(
+            reserva_id=reserve_id,
+            item_id=item_data.item_id,
+            tipo=item_data.tipo,
+            quantidade=item_data.quantidade,
+            preco_unitario=item_data.preco_unitario,
+            preco_total=item_data.preco_total,
+            desconto=item_data.desconto
+        )
+
+        db.add(item)
+
+        new_value = item_data.preco_total - item_data.desconto
+        reserve.valor_total += new_value
+
+        db.commit()
+        db.refresh(item)
+
+        return item
     
 item_reserva_service = ItemReservaService()
