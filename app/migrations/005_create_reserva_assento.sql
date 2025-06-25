affd881c13878db37870fad14e8c2fe854d22beb
Weslin-0101 <wesleeylirac12@gmail.com>
2025-06-24 22:04:27 -0300

    feat(app): creating 005 migration

---
+++ app/migrations/005_create_reserva_assento.sql
@@ -0,0 +1,25 @@
+BEGIN;
+
+ALTER TABLE public.reserva
+    DROP COLUMN IF EXISTS assentos;
+
+ALTER TABLE public.reserva
+    ALTER COLUMN metodo_pagamento DROP NOT NULL;
+
+UPDATE public.reserva SET status = 'confirmado' WHERE status = 'confirmado';
+UPDATE public.reserva SET status = 'expirada' WHERE status = 'expirado';
+
+ALTER TABLE public.reserva
+    DROP CONSTRAINT IF EXISTS reserva_status_check;
+
+ALTER TABLE public.reserva
+    ADD CONSTRAINT reserva_status_check CHECK (status IN ('pendente', 'confirmada', 'cancelada', 'expirada'));
+
+ALTER TABLE public.assento_sala
+    ADD COLUMN IF NOT EXISTS preco_adicional DECIMAL(8, 2) NOT NULL DEFAULT 0.0;
+
+DROP TABLE IF EXISTS public.reserva_assento;
+
+DROP TABLE IF EXISTS public.assento;
+
+COMMIT;
