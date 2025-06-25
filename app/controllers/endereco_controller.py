57cc6f173224d068683cd10f1aff844194a62125
Weslin-0101 <wesleeylirac12@gmail.com>
2025-06-25 10:06:30 -0300

    feat(app): applying the dependency to be admin to use the address routes

--- app/controllers/endereco_controller.py
+++ app/controllers/endereco_controller.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, Query, status
 from sqlalchemy.orm import Session
 
 from app.database import get_db
+from app.dependencies.auth import get_current_admin_user
 from app.models.schemas.endereco_schema import EnderecoCreate, EnderecoResponse, EnderecoUpdate
 from app.service.endereco_service import endereco_service
 
@@ -22,7 +23,8 @@ router = APIRouter(
 )
 async def create_address(
     address: EnderecoCreate,
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    current_user = Depends(get_current_admin_user)
 ):
     return endereco_service.create_endereco(db, address)
 
@@ -33,7 +35,11 @@ async def create_address(
     summary="Obtém endereço por ID",
     description="Obtém os detalhes de um endereço específico pelo ID fornecido. Retorna um erro 404 se o endereço não for encontrado.",
 )
-async def get_address_by_id(usuario_id: int, db: Session = Depends(get_db)):
+async def get_address_by_id(
+        usuario_id: int,
+        db: Session = Depends(get_db),
+        current_user = Depends(get_current_admin_user)
+    ):
     db_endereco = endereco_service.get_address_by_id(db, usuario_id)
 
     if not db_endereco:
@@ -51,7 +57,11 @@ async def get_address_by_id(usuario_id: int, db: Session = Depends(get_db)):
     summary="Obtém endereço por CEP",
     description="Obtém os detalhes de um endereço específico pelo CEP fornecido. Retorna um erro 404 se o endereço não for encontrado.",
 )
-async def get_address_by_cep(cep: str, db: Session = Depends(get_db)):
+async def get_address_by_cep(
+        cep: str,
+        db: Session = Depends(get_db),
+        current_user = Depends(get_current_admin_user)
+    ):
     db_endereco = endereco_service.get_address_by_cep(db, cep)
 
     if not db_endereco:
@@ -73,6 +83,7 @@ async def get_addresses(
     skip: int = Query(0, ge=0, description="Número de registros a serem pulados"),
     limit: int = Query(100, ge=1, le=1000, description="Número máximo de registros a serem retornados"), 
     db: Session = Depends(get_db),
+    current_user = Depends(get_current_admin_user)
 ):
     return endereco_service.get_all(db, skip=skip, limit=limit)
 
@@ -86,7 +97,8 @@ async def get_addresses(
 async def update_address(
     address_id: int,
     address_data: EnderecoUpdate,
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    current_user = Depends(get_current_admin_user)
 ):
     db_address = endereco_service.update_address(db, address_id, address_data)
 
@@ -106,7 +118,8 @@ async def update_address(
 )
 async def delete_address(
     address_id: int,
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    current_user = Depends(get_current_admin_user)
 ):
     success = endereco_service.delete_address(db, address_id)
 
