dc2851ca570055432cb7b489828e6c62f70c4f9e
Weslin-0101 <wesleeylirac12@gmail.com>
2025-07-18 23:36:47 -0300

    feat(app): adding the URL database

--- app/config.py
+++ app/config.py
@@ -16,8 +16,8 @@ class Settings(BaseSettings):
     PROJECT_VERSION: str = "1.0.0"
     PROJECT_DESCRIPTION: str = "API para gerenciamento de cinema"
 
-    #DATABASE_URL: str = "postgresql://fastapi_db_cenc_user:FmYrITLWvd3CYku92acH6zUut13IwRPi@dpg-d1tf4smr433s73dmb0lg-a/fastapi_db_cenc"
-    DATABASE_URL: str = "postgresql://postgres:postgres@db:5432/fastapi_db"
+    DATABASE_URL: str = "postgresql://fastapi_db_cenc_user:FmYrITLWvd3CYku92acH6zUut13IwRPi@dpg-d1tf4smr433s73dmb0lg-a/fastapi_db_cenc"
+    #DATABASE_URL: str = "postgresql://postgres:postgres@db:5432/fastapi_db"
 
     SECRET_KEY: str = "your_secret_key"
     ALGORITHM: str = "HS256"
